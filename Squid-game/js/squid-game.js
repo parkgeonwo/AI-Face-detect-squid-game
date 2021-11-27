fe47b9ca7165b1dff6941c19060257f4c9093406
Geonwoo Park <parkkeonwo@naver.com>
2021-11-27 18:26:05 +0900

    Add files via upload

---
+++ Squid-game/js/squid-game.js
@@ -0,0 +1,44 @@
+function readURL(input) {
+    if (input.files && input.files[0]) {
+  
+      var reader = new FileReader();
+  
+      reader.onload = function(e) {
+        $('.image-upload-wrap').hide();
+        $('.sub-ment').hide();
+        $('.sub-ment2').hide();
+        $('.cards').hide();
+
+        $('.file-upload-image').attr('src', e.target.result);
+        
+
+  
+        $('.image-title').html(input.files[0].name);
+      };
+  
+      reader.readAsDataURL(input.files[0]);
+      init().then(()=>{
+        predict();
+        $('.file-upload-content').show();
+        $('.file-upload-image').show();
+        $('.sub-ment3').show();
+        $('.sub-ment4').show();
+        $('.cards2').show();
+        $('.new-btn').show();
+      });
+    } else {
+      removeUpload();
+    }
+  }
+  
+  function removeUpload() {
+    $('.file-upload-input').replaceWith($('.file-upload-input').clone());
+    $('.file-upload-content').hide();
+    $('.image-upload-wrap').show();
+  }
+  $('.image-upload-wrap').bind('dragover', function () {
+          $('.image-upload-wrap').addClass('image-dropping');
+      });
+      $('.image-upload-wrap').bind('dragleave', function () {
+          $('.image-upload-wrap').removeClass('image-dropping');
+  });
